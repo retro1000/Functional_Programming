8857d398a4ef56406e333dcdb46857ad470209ea
retro1000 <damithajayawardana678@gmail.com>
2023-07-24 16:32:30 +0530

    delete files

--- 3.scala
+++
@@ -1,6 +0,0 @@
-
-def sum(n:Int):Int=if (n != 1) n+sum(n-1) else 1;
-
-def main(args: Array[String]):Unit={
-    println(sum(5));
-}
--- 4.scala
+++
@@ -1,5 +0,0 @@
-def evenOrOdd(n:Int):List[(String, Int)]=if(n!=1) evenOrOdd(n-1):+(if(n%2==0) "Even" else "Odd", n) else List(("Odd", 1))
-
-def main(args: Array[String]):Unit={
-    println(evenOrOdd(4));
-}
