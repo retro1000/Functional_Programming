ff28b6b1dc615fb94be9cbdd4e59a33e6fea71dd
retro1000 <damithajayawardana678@gmail.com>
2023-08-22 02:49:22 +0530

    t9_2

--- tutorial_9/2.scala
+++ tutorial_9/2.scala
@@ -1,3 +1,12 @@
+class Rational(val n:Int, val d:Int){
+    val numerator = n
+    val denominator = d
+
+    def sub(number:Rational):Rational = new Rational(numerator*number.denominator-number.numerator*denominator, denominator*number.denominator)
+}
+
 def main(args: Array[String]):Unit={
-    
+    val numberList:List[Rational] = List(new Rational(3, 4), new Rational(5, 8), new Rational(2, 7))
+    val result = numberList.reduce((i, j)=>i.sub(j))
+    println("Numerator: "+result.numerator+"   Denominator: "+result.denominator)
 }
