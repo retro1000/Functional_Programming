8d647ccc9a07dfda666d07312557d728478da4d9
retro1000 <136360020+retro1000@users.noreply.github.com>
2023-09-19 08:38:14 +0530

    Update 2.scala

--- tutorial_7/2.scala
+++ tutorial_7/2.scala
@@ -1,14 +1,7 @@
-package Q2
-
-object Q2 {
-
-  def calculateSquare ( li: List[Int]): List[Int]={
-     val sqe = li.map((x:Int) => x*x)
-    sqe
-  }
-  def main(args:Array[String]): Unit ={
-     val num = List(1,2,3,4,5,6,7,8,9)
-    val out = calculateSquare(num)
-    println("Output = " + out)
-  }
+def calculateSquare ( li: List[Int]): List[Int]={
+  val sqe = li.map((x:Int) => x*x)
+}
+def main(args:Array[String]): Unit ={
+  val num = List(1,2,3,4,5,6,7,8,9)
+  println(calculateSquare(num))
 }
