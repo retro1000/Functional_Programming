ea27e88e129273f9276816a981791763cb849dbc
retro1000 <136360020+retro1000@users.noreply.github.com>
2023-09-19 08:42:43 +0530

    Delete tutorial_7/2.scala

--- tutorial_7/2.scala
+++
@@ -1,7 +0,0 @@
-def calculateSquare ( li: List[Int]): List[Int]={
-  val sqe = li.map((x:Int) => x*x)
-}
-def main(args:Array[String]): Unit ={
-  val num = List(1,2,3,4,5,6,7,8,9)
-  println(calculateSquare(num))
-}
